b238b109b6e5a29d4bf072128b3da2c8c7d7cbc8
Le0xxx <dezhurovitdidant@gmail.com>
2025-01-11 02:56:35 +0500

    modified:   index.html modified:   main.js modified:   stily.css

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+const menuItems = document.querySelectorAll('#menu li');
+const ingredientsContainer = document.getElementById('ingredients');
+
+const ingredientsData = {
+    classic: ['Лаваш, мясо, лук, капуста, соус', 'Цена: 250₽'],
+    spicy: ['Лаваш, острый фарш, лук, капуста, острый соус', 'Цена: 270₽'],
+    chicken: ['Лаваш, курица, лук, капуста, соус', 'Цена: 280₽']
+};
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        const shaurmaType = item.dataset.shaurma;
+        ingredientsContainer.innerHTML = ingredientsData[shaurmaType].join('<br>');
+        ingredientsContainer.style.display = 'block';
+    });
+});
+
 
