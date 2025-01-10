19d07291c497a50c1f241334f3e2596da948e475
Le0xxx <dezhurovitdidant@gmail.com>
2025-01-11 03:29:45 +0500

    new file:   images/chicken-shaurma.jpg new file:   images/classic-shaurma.jpg modified:   images/header-image.jpg modified:   images/location-image.jpg new file:   images/spicy-shaurma.jpg modified:   index.html modified:   script.js modified:   style.css

--- script.js
+++ script.js
@@ -2,17 +2,26 @@ const menuItems = document.querySelectorAll('#menu li');
 const ingredientsContainer = document.getElementById('ingredients');
 
 const ingredientsData = {
-    classic: ['Лаваш, мясо, лук, капуста, соус', 'Цена: 250₽'],
-    spicy: ['Лаваш, острый фарш, лук, капуста, острый соус', 'Цена: 270₽'],
-    chicken: ['Лаваш, курица, лук, капуста, соус', 'Цена: 280₽']
+    classic: {
+        text: ['Лаваш, мясо, лук, капуста, соус', 'Цена: 250₽'],
+        image: 'images/classic-shaurma.jpg' // Путь к изображению классической шаурмы
+    },
+    spicy: {
+        text: ['Лаваш, острый фарш, лук, капуста, острый соус', 'Цена: 270₽'],
+        image: 'images/spicy-shaurma.jpg' // Путь к изображению острой шаурмы
+    },
+    chicken: {
+        text: ['Лаваш, курица, лук, капуста, соус', 'Цена: 280₽'],
+        image: 'images/chicken-shaurma.jpg' // Путь к изображению шаурмы с курицей
+    }
 };
 
 menuItems.forEach(item => {
     item.addEventListener('click', () => {
         const shaurmaType = item.dataset.shaurma;
-        ingredientsContainer.innerHTML = ingredientsData[shaurmaType].join('<br>');
+        let ingredientsHTML = `<img src="${ingredientsData[shaurmaType].image}" alt="${shaurmaType} шаурма"><br>`;
+        ingredientsHTML += ingredientsData[shaurmaType].text.join('<br>');
+        ingredientsContainer.innerHTML = ingredientsHTML;
         ingredientsContainer.style.display = 'block';
     });
 });
-
-
